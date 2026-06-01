cbe3898a4c6ddaa4b9b692e9b9f74f3018f78042
gurmeetui <uideveloper@mlgs.in>
2026-06-01 12:46:34 +0530

    project-setup

---
+++ src/components/Home/HomePage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+const HomePage = () => {
+  return (
+    <Box sx={{
+      background: 'var(--primary-color)',
+      height: '100vh'
+    }}>
+      <Typography variant='h1'>
+        Home Page Component Based
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit quidem eaque ab minima nisi pariatur voluptatem, a architecto adipisci veniam eum ea praesentium error sequi mollitia, voluptatum fuga ut reiciendis ducimus, iure earum inventore dolorem. Corrupti deserunt dolorem voluptas inventore.
+      </Typography>
+    </Box>
+  )
+}
+
+export default HomePage
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import HomePage from '../components/Home/HomePage'
+
 const Home = () => {
   return (
-    <Box sx={{
-      background: 'var(--primary-color)',
-      height: '100vh'
-    }}>
-      <Typography variant='h1'>
-        Home Page
-      </Typography>
-      <Typography>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit quidem eaque ab minima nisi pariatur voluptatem, a architecto adipisci veniam eum ea praesentium error sequi mollitia, voluptatum fuga ut reiciendis ducimus, iure earum inventore dolorem. Corrupti deserunt dolorem voluptas inventore.
-      </Typography>
-    </Box>
+    <>
+      <HomePage />
+    </>
   )
 }
 
